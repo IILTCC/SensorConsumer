cbeace4bc6bbb83cdfbba6583fa3bbe100e5d94b
Tomer <tomer.cohen243@gmail.com>
2024-12-18 14:40:44 +0200

    [feat] project init

---
+++ SensorConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SensorConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
