188da5a2752648c27ea086637f7ca496d004e46d
Tomer <tomer.cohen243@gmail.com>
2025-01-08 09:22:49 +0200

    [bugfix] final clean

---
+++ SensorConsumer/SensorConsumer.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+using MongoConsumerLibary.KafkaConsumer;
+using SensorConsumer.AppSettings;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SensorConsumer
+{
+    class SensorConsumer
+    {
+        private readonly KafkaSettings _kafkaSettings;
+        private readonly KafkaConnection _kafkaConnection;
+        private readonly HttpConnection _httpConnection;
+        public SensorConsumer()
+        {
+            ConfigProvider configProvider = ConfigProvider.Instance;
+            _kafkaSettings = configProvider.ProvideKafkaSettings();
+            _kafkaConnection = new KafkaConnection(_kafkaSettings);
+            _httpConnection = new HttpConnection();
+        }
+
+        public List<string> InitializeTopicNames()
+        {
+            List<string> topicNames = new List<string>();
+            foreach (string topic in _kafkaSettings.KafkaTopics)
+                topicNames.Add(topic);
+
+            return topicNames;
+        }
+        public async Task StartConsumer()
+        {
+            _kafkaConnection.WaitForKafkaConnection();
+            IConsumer<Ignore, string> consumer = _kafkaConnection.Consumer(InitializeTopicNames());
+            CancellationToken cancellationToken = _kafkaConnection.CancellationToken(consumer);
+            while (true)
+            {
+                try
+                {
+                    ConsumeResult<Ignore, string> consumerResult = consumer.Consume(cancellationToken);
+                    await _httpConnection.SendRequestAsync(consumerResult.Message.Value); 
+                }
+                catch (KafkaException e)
+                {
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+    }
+    
+}
